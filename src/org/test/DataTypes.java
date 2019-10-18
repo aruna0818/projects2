ae356deb23381a6c76e6cfde204a0d4fa8bcbbc2
aruna0818 <arunachalam3452@gmail.com>
2019-10-18 09:32:53 +0530

    project

---
+++ src/org/test/DataTypes.java
@@ -0,0 +1,35 @@
+package org.test;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		byte age=20;
+		System.out.println(" age is "+ age);
+		
+		short pin=1214;
+		System.out.println(" pin is "+ pin);
+		
+		int id=91899;
+		System.out.println(" id is "+id);
+		
+		long phno=9781714152l;
+		System.out.println(" phno is "+phno);
+		
+		float sal=451240.25f;
+		System.out.println(" sal is "+sal);
+	
+		double ctc=125154141.254;
+		System.out.println(" ctc is "+ctc);
+		
+		char block='A';
+		System.out.println(" block is "+block);
+		
+		String name="Arunachalam";
+		System.out.println(" name is "+name);
+		
+		boolean isemployee=true;
+		System.out.println( isemployee );
+		
+	}
+
+	
+}
